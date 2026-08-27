2e7146f8197f4ccb122cc4f1934477845205f994
MohammedFahad60 <mohammedfahadfahad60@gmail.com>
2026-08-27 13:20:50 +0530

    Fix production build

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import PageTransition from "./components/PageTransition";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer";
-import PageTransition from "./components/PageTransition";
-
-
-function Page({ title }: { title: string }) {
-  return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "#070707",
-        color: "white",
-        padding: "150px 20px",
-      }}
-    >
-      <h1>{title}</h1>
-    </main>
-  );
-}
 
 export default function App() {
   return (
@@ -30,7 +16,7 @@ export default function App() {
       <Navbar />
 
       <PageTransition />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
